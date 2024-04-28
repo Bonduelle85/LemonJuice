73bff1801e042e4e3d01b4daca87617e82781c5c
Anton Gorokhov <bonduelle85@gmail.com>
2024-04-28 14:10:28 +0300

    MainActivity, viewBinding

--- app/src/main/java/com/example/lemonjuice/MainActivity.kt
+++ app/src/main/java/com/example/lemonjuice/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.lemonjuice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lemonjuice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel = GameViewModel()
+
+        binding.actionButton.setOnClickListener {
+            val uiState: UiState = viewModel.nextStep()
+            uiState.update(binding)
+        }
+
+        binding.actionImageButton.setOnClickListener {
+            val uiState: UiState = viewModel.squeeze()
+            uiState.update(binding)
+        }
+
+        val uiState: UiState = viewModel.init()
+        uiState.update(binding)
     }
 }
